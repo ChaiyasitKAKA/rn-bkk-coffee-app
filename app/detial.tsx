7520b9c6e2b4056baec44142123f76c0cb2d372a
DevNobphakao <dev.nobphakao@gmail.com>
2026-02-04 16:02:45 +0700

    coffee

---
+++ app/detial.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Detial() {
+    return (
+        <View>
+            <Text>Detial</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
